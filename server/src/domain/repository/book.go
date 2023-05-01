4990a6f9e481586e76c42fad92aad163397935fb
Natty0404 <106375669+Natty0404@users.noreply.github.co>
2023-05-01 17:36:12 +0900

    refactor: repositoryをinterfaceの定義のみに修正

--- server/src/domain/repository/book.go
+++ server/src/domain/repository/book.go
@@ -2,53 +2,10 @@
 package repository
 
 import (
-	"../model"
+	"test-app/domain/model"
 )
 
-type BookService struct{}
-
-func (BookService) SetBook(book *model.Book) error {
-	_, err := DbEngine.Insert(book)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (BookService) GetBookList() []model.Book {
-	tests := make([]model.Book, 0)
-	err := DbEngine.Distinct("id", "title", "content").Limit(10, 0).Find(&tests)
-	if err != nil {
-		panic(err)
-	}
-	return tests
-}
-
-func (b *BookService) GetBookByTitle(title string) (*model.Book, error) {
-	book := new(model.Book)
-	has, err := DbEngine.Where("title = ?", title).Get(book)
-	if err != nil {
-		return nil, err
-	}
-	if !has {
-		return nil, nil
-	}
-	return book, nil
-}
-
-func (BookService) UpdateBook(newBook *model.Book) error {
-	_, err := DbEngine.Id(newBook.Id).Update(newBook)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (BookService) DeleteBook(id int) error {
-	book := new(model.Book)
-	_, err := DbEngine.Id(id).Delete(book)
-	if err != nil {
-		return err
-	}
-	return nil
+type Book interface {
+	insert(id, title, content string) error
+	get(title string) model.Book
 }
