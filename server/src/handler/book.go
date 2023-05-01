1cb6c654c16cd37ecfdaa43134a0a7e543cee3c3
Natty0404 <106375669+Natty0404@users.noreply.github.co>
2023-05-01 18:14:29 +0900

    refactor: handlerでusecaseを呼び出すよう修正

--- server/src/handler/book.go
+++ server/src/handler/book.go
@@ -4,92 +4,71 @@ package handler
 import (
 	// "fmt"
 	"net/http"
-	"strconv"
 
-	"../domain/service"
-
-	"../model"
+	"test-app/usecase"
 
 	"github.com/gin-gonic/gin"
 )
 
+// usecaseのsetBookを呼び出す
 func BookAdd(c *gin.Context) {
-	book := model.Book{}
-	err := c.Bind(&book)
-	if err != nil {
-		c.String(http.StatusBadRequest, "Bad request")
-		return
-	}
-
-	// bookService := service.BookService{}
-
-	// usecaseへ移動
-	// 同じタイトルの書籍が存在する場合は400エラーを返す
-	// existingBook, err := bookService.GetBookByTitle(book.Title)
-	// if err != nil {
-	// 	c.String(http.StatusInternalServerError, "Server Error")
-	// 	return
-	// }
-	// if existingBook != nil {
-	// 	c.String(http.StatusBadRequest, "Book with the same title already exists")
-	// 	return
-	// }
-
-	// err = bookService.SetBook(&book)
-	// if err != nil {
-	// 	c.String(http.StatusInternalServerError, "Server Error")
-	// 	return
-	// }
-
-	// コンソールに出力
-	// fmt.Println("テスト")
-
-	c.JSON(http.StatusCreated, gin.H{
-		"status": "ok",
-	})
-}
-
-func BookList(c *gin.Context) {
-	bookService := service.BookService{}
-	BookLists := bookService.GetBookList()
-	c.JSONP(http.StatusOK, gin.H{
-		"message": "ok",
-		"data":    BookLists,
-	})
-}
-
-func BookUpdate(c *gin.Context) {
-	book := model.Book{}
-	err := c.Bind(&book)
-	if err != nil {
-		c.String(http.StatusBadRequest, "Bad request")
-		return
-	}
-	bookService := service.BookService{}
-	err = bookService.UpdateBook(&book)
+	bookService := usecase.BookService{}
+	book, err := bookService.SetBook(c.PostForm("title"))
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Server Error")
 		return
 	}
+	if book != nil {
+		c.String(http.StatusBadRequest, "Bad request")
+		return
+	}
 	c.JSON(http.StatusCreated, gin.H{
 		"status": "ok",
 	})
 }
 
-func BookDelete(c *gin.Context) {
-	id := c.PostForm("id")
-	intId, err := strconv.ParseInt(id, 10, 0)
-	if err != nil {
-		c.String(http.StatusBadRequest, "Bad request")
-		return
-	}
-	bookService := service.BookService{}
-	err = bookService.DeleteBook(int(intId))
-	if err != nil {
-		c.String(http.StatusInternalServerError, "Server Error")
-		return
-	}
-	c.JSON(http.StatusCreated, gin.H{
-		"status": "ok",
-	})
-}
+// 一旦コメントアウト
+// func BookList(c *gin.Context) {
+// 	bookService := service.BookService{}
+// 	BookLists := bookService.GetBookList()
+// 	c.JSONP(http.StatusOK, gin.H{
+// 		"message": "ok",
+// 		"data":    BookLists,
+// 	})
+// }
+
+// func BookUpdate(c *gin.Context) {
+// 	book := model.Book{}
+// 	err := c.Bind(&book)
+// 	if err != nil {
+// 		c.String(http.StatusBadRequest, "Bad request")
+// 		return
+// 	}
+// 	bookService := service.BookService{}
+// 	err = bookService.UpdateBook(&book)
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, "Server Error")
+// 		return
+// 	}
+// 	c.JSON(http.StatusCreated, gin.H{
+// 		"status": "ok",
+// 	})
+// }
+
+// func BookDelete(c *gin.Context) {
+// 	id := c.PostForm("id")
+// 	intId, err := strconv.ParseInt(id, 10, 0)
+// 	if err != nil {
+// 		c.String(http.StatusBadRequest, "Bad request")
+// 		return
+// 	}
+// 	bookService := service.BookService{}
+// 	err = bookService.DeleteBook(int(intId))
+// 	if err != nil {
+// 		c.String(http.StatusInternalServerError, "Server Error")
+// 		return
+// 	}
+// 	c.JSON(http.StatusCreated, gin.H{
+// 		"status": "ok",
+// 	})
+// }
