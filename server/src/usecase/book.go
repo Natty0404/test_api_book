64527ede87e2ec87f8866ff005e03f0274061d19
Natty0404 <106375669+Natty0404@users.noreply.github.co>
2023-05-26 17:39:26 +0900

    refactor: 404 not foundを返す処理に変更

--- server/src/usecase/book.go
+++ server/src/usecase/book.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"test-app/domain/repository"
 
 	"github.com/go-xorm/xorm"
@@ -13,12 +14,12 @@ type BookService struct{}
 // 同じタイトルの書籍が存在する場合は400エラーを返す
 func (BookService) SetBook(title string) (*repository.Book, error) {
 	book := new(repository.Book)
-	has, err := DbEngine.Where("title = ?", title).Get(book)
+	books, err := DbEngine.Where("title = ?", title).Get(book)
 	if err != nil {
 		return nil, err
 	}
-	if !has {
-		return nil, nil
+	if !books {
+		return nil, fmt.Errorf("book with title '%s' not found", title)
 	}
 	return book, nil
 }
